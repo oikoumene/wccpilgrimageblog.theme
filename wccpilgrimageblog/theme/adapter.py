c0aa21b75e16d31a8aa89f828454044cc2d86899
sescatalan <sandrea@afterfivetech.com>
2015-04-20 13:12:22 +0800

    added user_biography field(richtext), hid the default description on user preferences of the plone

--- wccpilgrimageblog/theme/adapter.py
+++ wccpilgrimageblog/theme/adapter.py
@@ -10,3 +10,9 @@ class EnhancedUserDataSchemaAdapter(UserDataPanelAdapter):
     def set_twitter_username(self, value):
         return self.context.setMemberProperties({'twitter_username': value})
     twitter_username = property(get_twitter_username, set_twitter_username)
+
+    def get_user_biography(self):
+        return self.context.getProperty('user_biography', '')
+    def set_user_biography(self, value):
+        return self.context.setMemberProperties({'user_biography': value})
+    user_biography = property(get_user_biography, set_user_biography)
