4693ea7596e5062e8b32a9c129746e059198e2b1
sescatalan <sandrea@afterfivetech.com>
2015-04-22 13:27:00 +0800

    unchecked notification email on commentextender

--- wccpilgrimageblog/theme/commentextender.py
+++ wccpilgrimageblog/theme/commentextender.py
@@ -4,11 +4,11 @@ from plone.directives import form
 from zope import schema
 from plone.app.discussion.interfaces import IComment
 
-class defaultUserNotification(CommentForm):
-    def __init__(self, context, request):
-        super(defaultUserNotification, self).__init__(context, request)
+# class defaultUserNotification(CommentForm):
+#     def __init__(self, context, request):
+#         super(defaultUserNotification, self).__init__(context, request)
         
 
-@form.default_value(field=IComment['user_notification'])
-def defaultValue(self):
-    return False
+# @form.default_value(field=IComment['user_notification'])
+# def defaultValue(self):
+#     return False
