aba81f9251fbdb10d221cd47c4901437821b1761
glenn <glenn@glenn-Lenovo-G480>
2015-07-10 16:02:19 +0800

    added new classes

--- wccpilgrimageblog/theme/interfaces.py
+++ wccpilgrimageblog/theme/interfaces.py
@@ -1,5 +1,17 @@
 from zope.interface import Interface
+#from quintagroup.z3cform.captcha import Captcha, CaptchaWidgetFactory
+from wccpilgrimageblog.theme import MessageFactory as _
+from quintagroup.formlib.captcha import Captcha
 
 class IProductSpecific(Interface):
     pass
 
+
+class IExtendRegistrationForm(Interface):
+    """Marker interface for my custom registration form
+    """
+
+class ICaptchaSchema(Interface):
+    captcha = Captcha(
+        title=_(u'Type the code'),
+        description=_(u'Type the code from the picture shown below.'))
